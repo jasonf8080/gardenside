15c9305cfe30307327c667d9709f8d045d6c3f4e
jasonf8080 <your-email@example.com>
2026-06-13 17:02:10 -0400

    add gradient effect on cards

--- client/src/components/Home/Categories.jsx
+++ client/src/components/Home/Categories.jsx
@@ -15,7 +15,7 @@ const Categories = () => {
     return (
         <section 
             ref={ref} 
-            className='relative pt-[100px] md:pt-[150px]'
+            className='relative pt-[120px] md:pt-[150px]'
         >
             <img className='absolute top-0 left-0 w-full h-full z-1' src="/images/categories.JPEG" alt="" />
 <div
@@ -26,7 +26,7 @@ const Categories = () => {
   }}
 />
             <div className="container flex justify-center items-center flex-col pt-8 pb-2 md:pt-24 md:pb-24  z-5 relative">
-            <h1 className='text-2xl md:text-4xl mb-3 md:mb-6 font-heading'>Shop our collection</h1>
+            <h1 className='text-2xl md:text-4xl mb-2 md:mb-6 font-heading'>Shop our collection</h1>
             {/* <img className='max-w-[80px] md:max-w-[100px]' src="https://png.pngtree.com/png-clipart/20240109/original/pngtree-cartoon-croissant-png-image_14061296.png" alt="pastry" /> */}
             <p className='text-lg md:text-2xl text-center'>From homemade pastries to savory bites and perfectly brewed coffee explore out handcrafted favorites</p>
             <div className='w-full py-6 flex gap-3 md:gap-5 mb-8 overflow-scroll '>
@@ -38,7 +38,7 @@ const Categories = () => {
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
                             transition={{duration: 0.5, delay: parseFloat(`0.${index * 2}`)}}
-                            className="cursor-pointer bg-[#fffbf3]  ">
+                            className="cursor-pointer bg-[#fffbf3]">
                                 <motion.img 
                                 src={item.image} 
                                 alt={item.name} 
@@ -48,7 +48,8 @@ const Categories = () => {
                                 animate={{ opacity: 1 }}
                                 transition={{ duration: 0.5, delay: index * 0.1 + 0.5 }}
                                 />
-                                <div className='p-5 pb-8 flex flex-col justify-center items-center text-center'>
+                                <div className='p-5 pb-8 flex flex-col justify-center items-center text-center relative'>
+                                    <div className="absolute w-full h-[50px] top-[-50px] left-0 bg-gradient-to-b from-transparent to-[#fffbf3]"></div>
                                     <h3 className='font-heading text-center text-2xl'>{item.name}</h3>
                                     <p className='text-lg mt-1 mb-3'>{item.content}</p>
                                     <button className="primary-button !bg-accent !border-accent !font-normal px-4 py-1">{`Shop ${item.name}`}</button>
